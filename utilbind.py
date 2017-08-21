96ef174ff9beebc5cca474fe274b96286f128e0a
grun <grunseid@gmail.com>
2017-08-21 14:21:53 -0700

    First floor checkin.

---
+++ utilbind.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+# Copyright _!_
+#
+# License _!_
+#
+# Original author: Ansgar Grunseid
+
+import curio
+
+def main():
+    print('sup')
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ utilities/password_generator/password_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+# Copyright _!_
+#
+# License _!_
+#
+# Original author: Ansgar Grunseid
+
+def generate_password():
+    return 'sup'
