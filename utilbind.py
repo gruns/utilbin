b331e70874664bfe8947ffa67dd582ffc06645e0
grun <grunseid@gmail.com>
2017-08-21 17:45:05 -0700

    Refactor.

--- utilbind.py
+++ utilbind.py
@@ -17,6 +17,7 @@ Options:
   --version    Show version.
   -h --help    Show this help information.
 """
+UTILITY_TIMEOUT = 5  # Seconds.
 UTILITIES_DIRECTORY = './utilities/'
 
 import os
@@ -25,16 +26,33 @@ from os.path import isfile, join as pjoin
 
 import curio
 from docopt import docopt
+from curio import timeout_after, TaskTimeout
 from curio.subprocess import CalledProcessError
 
+async def runUtilityCLI(exe):
+    success, stdout, stderr = await runUtility(exe)
+    if success and None not in [stdout, stderr]:
+        print(f'stdout: {stdout}')
+        print
+        print(f'stderr: {stderr}')
+
 async def runUtility(exe):
+    success, stdout, stderr = False, None, None
+
     try:
-        proc = await curio.subprocess.run(exe, stdout=PIPE, stderr=PIPE)
+        async with timeout_after(UTILITY_TIMEOUT):
+            proc = await curio.subprocess.run(exe, stdout=PIPE, stderr=PIPE)
+    except TaskTimeout:
+        print(f'Utility {exe} timed out after {UTILITY_TIMEOUT} seconds.')
     except CalledProcessError as e:
         print(f'Utility {exe} returned with non-zero retcode: {e.returncode}.')
-    stdout, stderr = proc.stdout.decode('utf8'), proc.stderr.decode('utf8')
-    print(f'stdout: {stdout}')
-    print(f'stderr: {stderr}')
+    else:
+        success = True
+        # TODO(grun): Handle received data intelligently -- not all utility
+        # output will be text (i.e. UTF-8), e.g. raw JPEG data.
+        stdout, stderr = proc.stdout.decode('utf8'), proc.stderr.decode('utf8')
+
+    return success, stdout, stderr
 
 def checkForUtility(utility):
     exePath = pjoin(UTILITIES_DIRECTORY, utility, utility)
@@ -49,7 +67,7 @@ def main():
         utility = args.get('<utility>')
         exe, exists = checkForUtility(utility)
         if exe and exists:
-            curio.run(runUtility, exe)
+            curio.run(runUtilityCLI, exe)
         else:
             print(f'Failed to find the utility "{utility}".')
 
